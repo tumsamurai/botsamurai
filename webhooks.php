7c6120d14426614245c60fb23745c5b86c2994cc
tumsamurai <45464957+tumsamurai@users.noreply.github.com>
2018-11-30 23:22:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'UtVtKyUXYQTybiUHkVjDtr/jjhk+1S83ehYXmkAFCAySM1Ivgipn+A630yIkD8ULWtQ80lnAb+6ljALWv+lMsVW4h3vsZYMz0n4/znsqIRnIG7NXHT80CMrUC8TC68kSkzLyB1e3i2Co0oNdcWPyXwdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
